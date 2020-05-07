7f717f716f421f88678a8ae243278fc22b5caf3d
jaketrent <trent.jake@gmail.com>
2020-05-07 16:58:11 -0600

    chore(site): regen

--- docs/_next/b6df7eae3050d46c6e5851a03bceb18b/app.js
+++ docs/_next/b6df7eae3050d46c6e5851a03bceb18b/app.js
@@ -8001,20 +8001,7 @@ webpackJsonp(
             k.default,
             null,
             a.default.createElement(P.default, null, 'Catch a preview'),
-            a.default.createElement(w.default, null),
-            a.default.createElement(
-              'div',
-              {
-                style: {
-                  display: 'flex',
-                  justifyContent: 'center',
-                  margin: '20px'
-                }
-              },
-              a.default.createElement('img', {
-                src: 'https://cdn.jaketrent.com/supertest.jpg'
-              })
-            )
+            a.default.createElement(w.default, null)
           ),
           a.default.createElement(
             k.default,
